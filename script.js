5b14d77e8671cc5ebad0798a763b11ec105d0483
rafaelTeixeiraDesigner <rafaelteixeiradesigner@gmail.com>
2022-07-23 17:08:48 -0300

    corrige fórmula

--- script.js
+++ script.js
@@ -17,18 +17,18 @@ function calcular () {
 
             let refriPP = ""
             if (duracao < 6)
-            refriPP = 600
+            refriPP = 500
             else
-            refriPP = 1000
+            refriPP = 600
 
             let carneTotal = ((inputAdultos.value * carnePP) + (inputCriancas.value * carnePP / 2)) /1000;
             let cervejaTotal = (Math.ceil((inputAdultos.value * 1700)/473)); 
-            let refriTotal = (Math.ceil(((inputAdultos.value * refriPP)/1000) + ((inputCriancas.value *refriPP)/1000))) ;
+            let refriTotal = (Math.ceil(((inputAdultos.value * refriPP)/2000) + ((inputCriancas.value *refriPP)/1000))) ;
 
-            resultContent.innerHTML = `<p id=resultFunction>Para fazer um <span id=colorRed>Churrasco Perfeito!</span> com <span id=colorWhite>${inputDuracao.value} horas</span> de duracao, para <span id=colorWhite>${inputAdultos.value} adultos</span> e <span id=colorWhite> ${inputCriancas.value} criancas</span>...
-            <br><br> Voce vai precisar de: <br><br> <span id=colorWhite2>${carneTotal}</span> kg de <span id=colorBlack>Carne</span>
-            <br> <span id=colorWhite2>${cervejaTotal}</span> latas de <span id=colorBlack>Cerveja</span>
-            <br> <span id=colorWhite2>${refriTotal}</span> <span id=colorBlack>refrigerantes</span> 2Lt.</span></p>`
+            resultContent.innerHTML = `<p id=resultFunction>Para fazer um <span id=colorRed>Churrasco Perfeito!</span> com <span id=colorWhite>${inputDuracao.value} hora(s)</span> de duração, para <span id=colorWhite>${inputAdultos.value} adulto(s)</span> e <span id=colorWhite> ${inputCriancas.value} criança(s)</span>...
+            <br><br> Você vai precisar de: <br><br> <span id=colorWhite2>${carneTotal}</span> kg(s) de <span id=colorBlack>Carne</span>
+            <br> <span id=colorWhite2>${cervejaTotal}</span> lata(s) de <span id=colorBlack>Cerveja</span>
+            <br> <span id=colorWhite2>${refriTotal}</span> <span id=colorBlack>refrigerante(s)</span> 2Lt.</span></p>`
 
             console.log("Carne total " + carneTotal + "(kg)")
             console.log("Cerveja total " + cervejaTotal + " (latões)")
